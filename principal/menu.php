867bbf01e247eaf3790595737347be9f8f3150bf
kristyyjesus <56432273+kristyyjesus@users.noreply.github.com>
2019-10-11 03:57:45 -0500

    Add files via upload

---
+++ principal/menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bienvenido</title>
+    <h4>Bienvenido a la pagina usuario</h4>
+    <a href="principal/salir.php"><button type="submit" class="btn btn-danger btn-block">Salir</button></a>
+</head>
+<body>
+    
+</body>
+</html>
